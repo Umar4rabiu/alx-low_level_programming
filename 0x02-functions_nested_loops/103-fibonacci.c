3860cab75baa1c71fbf099f037e8351bca82972a
Umar4rabiu <umar4mls@gmail.com>
2023-09-13 18:12:31 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,14 +13,13 @@ int main(void)
 	int i = 0;
 	long j = 1, k = 2, sum = k;
 
-
 	while (k + j < 4000000)
 	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		++i;
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
 	}
 	printf("%ld", sum);
 	return (0);
